fd267483618dd772c223c4a8b88f67df0176e082
johnmr24 <91611636+johnmr24@users.noreply.github.com>
2022-02-22 17:42:55 -0500

    Add files via upload

---
+++ Programming Assignment #3/Programming Exercise 10/Program.cs
@@ -0,0 +1,83 @@
+﻿//John Reichenbach
+//CS 285-001
+//9-22-2021
+//Programming Assignment #10
+
+using System;
+using static System.Console;
+
+namespace Programming_Exercise_10
+{
+    class Programming_Exercise_10
+    {
+        static void Main()
+        {
+            string address; //holds the address
+            double priorYearValue; //holds the prior year value
+            const double MILLAGE_RATE = 10.03; //holds the millage rate
+            const double EXEMPTION = 25000; //holds the exemption amount
+            const double NEW_VALUE_INCREASE = 0.027; //holds the new year increase amount
+
+            address = inputAddress(); //inputs the address
+            priorYearValue = inputValue(); //inputs the prior year value
+
+            Clear(); //clears the console
+
+            displayResults(priorYearValue, NEW_VALUE_INCREASE, MILLAGE_RATE, EXEMPTION, address); //displays the results
+        }
+
+        static string inputAddress() //inputs the address
+        {
+            string input;
+
+            Write("Enter your address: ");
+            input = ReadLine();
+
+            return input;
+
+        }
+
+        static double inputValue() //inputs the previous years value
+        {
+            string input;
+            double value;
+
+            Write("Enter the prior years value for your home: $");
+            input = ReadLine();
+            value = double.Parse(input);
+
+            return value;
+        }
+
+        static double calcNewValue(double prior_year_value, double new_value_increase) //calculates the current years value
+        {
+            double newValue = prior_year_value + (prior_year_value * new_value_increase); //adds the new year precentage increase to the previos year
+
+            return newValue;
+        }
+
+        static double calcTaxes(double new_year_value, double millage_rate, double exemption) //calculates the taxes
+        {
+            double millageTax;
+            new_year_value -= exemption; //subtracts the 25000 tax exemption
+            millageTax = new_year_value / 1000 * millage_rate; //millage tax is equal to the new years value divided by
+                                                               //$10.03 per $1000
+            return millageTax;
+        }
+
+        static void displayResults(double prior_year_value, double new_value_increase, //displays the results
+                                   double millage_rate, double exemption, string address)
+        {
+
+            WriteLine("{0,35}", address); //displays the address
+            WriteLine();
+            WriteLine("{0,-40}{1,15:c}", "Prior year's home value: ", prior_year_value); //displays the prior year's home value
+            WriteLine("{0,-40}{1,15:c}", "Current year's home value: ", calcNewValue(prior_year_value, //displays the current home value
+                                                                                     new_value_increase));
+            WriteLine();
+            WriteLine("{0,-40}{1,15:c}", "Proposed taxes for current year: ", calcTaxes( //displays the proposed taxes 
+                 calcNewValue(prior_year_value, new_value_increase), millage_rate, exemption));
+        }
+
+    }
+}
---
+++ Programming Assignment #3/Programming Exercise 9/Program.cs
@@ -0,0 +1,109 @@
+﻿//John Reichenbach
+//CS 285-001
+//9-22-2021
+//Programming Exercise #9
+
+using System;
+using static System.Console;
+
+namespace Programming_Exercise_9
+{
+    class Programming_Exercise_9
+    {
+
+        static void Main()
+        {
+            string employeeName;                 //holds the employee name
+            double sales;                        //holds the total sales amount
+            double commission;                   //holds the commission amount
+            const double COMMISSION_RATE = 0.07; //holds the commission rate
+            const double FED_TAX_RATE = 0.18;    //holds the federal tax rate
+            const double RETIREMENT_RATE = 0.15; //holds the retirement rate
+            const double SOC_SEC_RATE = 0.09;    //holds the social security rate
+
+            employeeName = inputName();                          //inputs the employee name
+            sales = inputSales();                                //inputs the total sales
+            commission = calcCommission(sales, COMMISSION_RATE); // calculates the commission
+            Clear();                                             //clear console
+
+            displayResults(employeeName, sales, commission, COMMISSION_RATE, FED_TAX_RATE,
+                           RETIREMENT_RATE, SOC_SEC_RATE); //displays results
+
+        }
+
+        static string inputName() //inputs the employee name
+        {
+            string input;
+
+            Write("Enter your name: ");
+            input = ReadLine();
+
+            return input;
+
+        }
+
+        static double inputSales() //inputs the total sales
+        {
+            string input;
+            double sales;
+
+            Write("Enter the sales amount: $");
+            input = ReadLine();
+            sales = double.Parse(input);
+
+            return sales;
+        }
+
+        static double calcCommission(double sales, double commission_rate) //calculates the commission earned
+        {
+            sales *= commission_rate;
+            return sales;
+        }
+
+        static double calcFedTax(double commission, double federal_rate) //calculates the total federal tax
+        {
+            commission *= federal_rate;
+            return commission;
+        }
+
+        static double calcRetirement(double commission, double retirement_rate) //calculates the retirement rate
+        {
+            commission *= retirement_rate;
+            return commission;
+        }
+
+        static double calcSocSec(double commission, double social_security_rate) //calculates the social security rate
+        {
+            commission *= social_security_rate;
+            return commission;
+        }
+
+        static double calcMoneyMade(double sales, double commission, double commission_rate, //calculates the total money made with deductions
+                                   double federal, double retirement, double social)
+        {
+            double moneyMade = (calcCommission(sales, commission_rate))         //adds the commission 
+                              - (calcFedTax(commission, federal))               //subtracts the federal tax
+                              - (calcRetirement(commission, retirement))        //subtracts the retirement
+                              - (calcSocSec(commission, social));               //subtracts the social security
+            return moneyMade;
+        }
+
+        static void displayResults(string name, double sale, double commission, double commission_rate, //displays results
+                                   double federal, double retirement, double social)
+        {
+            WriteLine("{0,35}", name); //displays name
+            WriteLine();
+            WriteLine("{0,-35}{1,15:c}", "Total Sales: ", sale); //displays total sales
+            WriteLine();
+            WriteLine("{0,-35}{1,15:c}", "Commission Recieved: ", commission); //displays commission
+            WriteLine("{0,-35}{1,15:c}", "Federal Tax Deducted: ", calcFedTax(commission, federal)); //displays federal tax
+            WriteLine("{0,-35}{1,15:c}", "Retirement Contribution Deducted: ", calcRetirement(commission, retirement)); //displays retirement
+            WriteLine("{0,-35}{1,15:c}", "Social Security Deduction: ", calcSocSec(commission, social)); //displays social security
+            WriteLine();
+            WriteLine("{0,-35}{1,15:c}", "Total Money Earned: ", calcMoneyMade(sale, commission, commission_rate, //displays total money made
+                                                                  federal, retirement, social));
+
+
+        }
+    }
+}
