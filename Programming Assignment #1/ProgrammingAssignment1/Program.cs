c403911982d69f7c3b6f444bfe6392ea9335459f
johnmr24 <91611636+johnmr24@users.noreply.github.com>
2022-02-22 17:41:32 -0500

    Add files via upload

---
+++ Programming Assignment #1/ProgrammingAssignment1/Program.cs
@@ -0,0 +1,18 @@
+﻿// John Reichenbach
+// Programming Assignment 1
+// CS 285
+// August 31
+
+using System;
+
+namespace ProgrammingAssignment1
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Hello World! My name is John Reichenbach."); //prints hello world and name
+            Console.WriteLine("In Spanish: Hello World! Mi nombre es John Reichenbach."); //prints hello world and name in spanish
+        }
+    }
+}
